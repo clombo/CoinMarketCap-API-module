6840eef135b603c92da2848fa8d9aa44fde98095
clombo <christo.lombard@live.co.za>
2017-09-29 11:46:12 +0200

    Added example code

--- example.py
+++ example.py
@@ -1,8 +1,20 @@
 from coincap import Market
 
-#ticker = Market.Global()
+#Ticker example
+ticker1 = Market.Ticker()
 
-#print ticker
+#Also takes additional arguments
+ticker2 = Market.Ticker(limit=1,convert='EUR')
+
+#For specific currency
+litecoin = Market.Ticker(id='litecoin')
+#or
+litecoin = Market.Ticker(id='litecoin',convert='EUR')
+
+#Global example
+gbl = Market.Global()
+#or
+gbl = Market.Global(convert='EUR')
 
 g = Market.Global()
 
