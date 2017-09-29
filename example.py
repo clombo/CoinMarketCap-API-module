c0470d962b769d5526d9e069b5c8fbd69359f2a6
clombo <christo.lombard@live.co.za>
2017-09-29 14:44:21 +0200

    Example code updated

--- example.py
+++ example.py
@@ -1,22 +1,33 @@
 from coincap import Market
 
+""" 
+Please note that this is example code and should not be run all at once.abs
+
+ If you would like to test it I suggest running and printing the data back for each section separately otherwise
+ you will be flooded with data.
+
+"""
+
 #Ticker example
 ticker1 = Market.Ticker()
 
+print ticker1
+
+print '\n'
+
 #Also takes additional arguments
 ticker2 = Market.Ticker(limit=1,convert='EUR')
+
+print ticker2
 
 #For specific currency
 litecoin = Market.Ticker(id='litecoin')
 #or
 litecoin = Market.Ticker(id='litecoin',convert='EUR')
 
+print litecoin
+
 #Global example
 gbl = Market.Global()
 #or
 gbl = Market.Global(convert='EUR')
-
-g = Market.Global()
-
-for key in g:
-    print "{}:{}\n".format(key,g[key])
